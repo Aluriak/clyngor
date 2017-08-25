ad116af2a1886f39f42d64e9a463f57d0c364983
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-25 16:04:58 +0200

    test: more tests for api and dirty input

--- clyngor/test/test_api.py
+++ clyngor/test/test_api.py
@@ -18,17 +18,21 @@ def test_api_asp():
     assert found == {'a×cd', 'b×de', 'ab×d'}
 
 
-def test_api_asp():
+def test_string_with_lot_of_crap():
+    dangerous_string = r'"\"1,3-dimethyl-2-[2-oxopropyl thio]imidazolium chloride\""'
+    for sol in clyngor.ASP('atom({}).'.format(dangerous_string)).careful_parsing:
+        assert len(sol) == 1
+        pred, args = next(iter(sol))
+        assert pred == 'atom'
+        assert len(args) == 1
+        assert args[0] == dangerous_string
 
-    answers = ASP("""
-    rel(a,(c;d)). rel(b,(d;e)).
-    obj(X):- rel(X,_) ; rel(X,Y): att(Y).
-    att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
-    :- not obj(X):obj(X).
-    :- not att(Y):att(Y).
-    """)
 
-    found = set()
-    for answer in answers.by_predicate.sorted.first_arg_only:
-        found.add(''.join(answer['obj']) + '×' + ''.join(answer['att']))
-    assert found == {'a×cd', 'b×de', 'ab×d'}
+def test_string_without_escaped_quotes():
+    dangerous_string = r'"1,3-dimethyl-2-[2-oxopropyl thio]imidazolium chloride"'
+    for sol in clyngor.ASP('atom({}).'.format(dangerous_string)).careful_parsing:
+        assert len(sol) == 1
+        pred, args = next(iter(sol))
+        assert pred == 'atom'
+        assert len(args) == 1
+        assert args[0] == dangerous_string
