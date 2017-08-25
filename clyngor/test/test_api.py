8887a0b50182079298e6e06726e270cae24d9d66
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-25 13:08:44 +0200

    new unit test for api

--- clyngor/test/test_api.py
+++ clyngor/test/test_api.py
@@ -16,3 +16,19 @@ def test_api_asp():
     for answer in answers.by_predicate.sorted.first_arg_only:
         found.add(''.join(answer['obj']) + '×' + ''.join(answer['att']))
     assert found == {'a×cd', 'b×de', 'ab×d'}
+
+
+def test_api_asp():
+
+    answers = ASP("""
+    rel(a,(c;d)). rel(b,(d;e)).
+    obj(X):- rel(X,_) ; rel(X,Y): att(Y).
+    att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
+    :- not obj(X):obj(X).
+    :- not att(Y):att(Y).
+    """)
+
+    found = set()
+    for answer in answers.by_predicate.sorted.first_arg_only:
+        found.add(''.join(answer['obj']) + '×' + ''.join(answer['att']))
+    assert found == {'a×cd', 'b×de', 'ab×d'}
