1ff5003c045593f06775475f5b3b799906a9a647
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-25 16:38:31 +0200

    main: run only if main script

--- clyngor/__main__.py
+++ clyngor/__main__.py
@@ -2,15 +2,12 @@
 from clyngor import ASP, solve
 
 
-# if __name__ == '__main__':
-answers = ASP("""
-rel(a,(c;d)). rel(b,(d;e)).
-obj(X):- rel(X,_) ; rel(X,Y): att(Y).
-att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
-:- not obj(X):obj(X).
-:- not att(Y):att(Y).
-""")
-for answer in answers.by_predicate.first_arg_only:
-# for answer in answers:
-    print(answer)
-    print('{' + ','.join(answer['obj']) + '} × {' + ','.join(answer['att']) + '}')
+    answers = ASP("""
+    rel(a,(c;d)). rel(b,(d;e)).
+    obj(X):- rel(X,_) ; rel(X,Y): att(Y).
+    att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
+    :- not obj(X):obj(X).
+    :- not att(Y):att(Y).
+    """)
+    for answer in answers.by_predicate.first_arg_only:
+        print('{' + ','.join(answer['obj']) + '} × {' + ','.join(answer['att']) + '}')
