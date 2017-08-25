39cfb03bb0ef9e528364cfc3db46e9a940da4788
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-25 10:34:32 +0200

    provides packaging, upload on pypi

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 t: tests
 tests:
 	python -m pytest -vv clyngor --doctest-module
+
+
+upload:
+	python setup.py sdist upload
+
+install:
+	yes y | pip uninstall clyngor
+	pip install clyngor
---
+++ setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#########################
+#       SETUP.PY        #
+#########################
+
+
+#########################
+# IMPORTS               #
+#########################
+import os
+from setuptools   import setup, find_packages
+from pip.req      import parse_requirements
+from pip.download import PipSession
+
+
+# access to the file at the package top level (like README)
+def path_to(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(path_to('requirements.txt'), session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+#########################
+# SETUP                 #
+#########################
+setup(
+    name = 'clyngor',
+    version = '0.0.2',
+    packages = find_packages(),
+    include_package_data = True,  # read the MANIFEST.in file
+    install_requires = reqs,
+
+    author = "lucas bourneuf",
+    author_email = "lucas.bourneuf@laposte.net",
+    description = "Python wrapper around Clingo/Answer Set Programming",
+    long_description = open(path_to('README.mkd')).read(),
+    keywords = "ASP clingo wrapper",
+    url = "https://github.com/aluriak/clyngor",
+
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: ASP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
