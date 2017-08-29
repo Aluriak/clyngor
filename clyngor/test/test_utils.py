3a08d1e3e7685badb2c8973e541339a37105f188
Aluriak <lucas.bourneuf@laposte.net>
2017-08-29 15:17:41 +0200

    utils: show that answers saving handle pyasp-like interface

--- clyngor/test/test_utils.py
+++ clyngor/test/test_utils.py
@@ -35,3 +35,14 @@ def test_file_saving_api_with_dict(asp_code):
     # must be the same as regular repr of answer sets
     answers = frozenset(ASP(asp_code))
     assert answers == read_answers
+
+
+def test_file_saving_api_with_as_pyasp(asp_code):
+    # create the read_answers from the dictionary repr of answer sets
+    as_dict_answers = tuple(ASP(asp_code).as_pyasp)
+    fname = utils.save_answers_in_file(as_dict_answers)
+    read_answers = frozenset(utils.load_answers_from_file(fname))
+
+    # must be the same as regular repr of answer sets
+    answers = frozenset(ASP(asp_code))
+    assert answers == read_answers
