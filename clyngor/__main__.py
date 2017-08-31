ae0b8bb51e7b02a276f64f97174638095198b7ef
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-31 19:12:39 +0200

    main: playground for development

--- clyngor/__main__.py
+++ clyngor/__main__.py
@@ -2,18 +2,16 @@
 from clyngor import ASP
 
 
+ASP_CODE = """
+rel(a,(c;d)). rel(b,(d;e)).
+obj(X):- rel(X,_) ; rel(X,Y): att(Y).
+att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
+:- not obj(X):obj(X).
+:- not att(Y):att(Y).
+"""
+
 
 if __name__ == '__main__':
-    answers = ASP("""
-    rel(a,(c;d)). rel(b,(d;e)).
-    obj(X):- rel(X,_) ; rel(X,Y): att(Y).
-    att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
-    :- not obj(X):obj(X).
-    :- not att(Y):att(Y).
-    """)
+    answers = ASP(ASP_CODE)
     for answer in answers.by_predicate.first_arg_only:
         print('{' + ','.join(answer['obj']) + '} × {' + ','.join(answer['att']) + '}')
-
-
-
-
