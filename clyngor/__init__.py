4cc49f78a10a9e6803e8f2efb67376469158c715
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-31 14:45:07 +0200

    init: expose Answers and command

--- clyngor/__init__.py
+++ clyngor/__init__.py
@@ -1,4 +1,5 @@
 CLINGO_BIN_PATH = 'clingo'
 
-from clyngor.solving import solve, clingo_version
+from clyngor.answers import Answers
+from clyngor.solving import solve, clingo_version, command
 from clyngor.inline import ASP
