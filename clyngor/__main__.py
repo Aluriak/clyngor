fa93a3fa4e3bb1cca387912b3b0c19c49a308a16
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-31 20:58:25 +0200

    main: use the dependancy graph generation

--- clyngor/__main__.py
+++ clyngor/__main__.py
@@ -1,5 +1,10 @@
 
+from timeit import timeit
+from pprint import pprint
+from functools import partial
+
 from clyngor import ASP
+from clyngor import asp_parsing
 
 
 ASP_CODE = """
@@ -10,8 +15,21 @@ att(Y):- rel(_,Y) ; rel(X,Y): obj(X).
 :- not att(Y):att(Y).
 """
 
+def time_efficiency():
+    func = partial(asp_parsing.program_to_dependancy_graph, ASP_CODE)
+    print('Perform 1000 graphs in', timeit(func, number=1000), 'seconds.')
+    # last time: 3.4s
+
 
 if __name__ == '__main__':
     answers = ASP(ASP_CODE)
     for answer in answers.by_predicate.first_arg_only:
         print('{' + ','.join(answer['obj']) + '} × {' + ','.join(answer['att']) + '}')
+    print()
+
+    print('Dependancy graph:')
+    pprint(asp_parsing.program_to_dependancy_graph(ASP_CODE))
+    print()
+
+    print('Benchmark:')
+    time_efficiency()
