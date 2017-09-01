93c4dca1811521505d2fe004a246ac6de3ccd402
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-01 13:12:03 +0200

    new ASP parser (because of new grammar)

--- clyngor/asp_parsing/parser.py
+++ clyngor/asp_parsing/parser.py
@@ -23,6 +23,12 @@ class CodeAsTuple(ap.PTNodeVisitor):
     def visit_args(self, node, children):
         return tuple(children)
 
+    def visit_multargs(self, node, children):
+        if len(children) > 1:
+            return ('disjunction', *children)
+        return tuple(children)
+
+
     def visit_text(self, node, children):
         text = tuple(children)[0]
         return 'text', str(text)
@@ -30,23 +36,48 @@ class CodeAsTuple(ap.PTNodeVisitor):
     def visit_ident(self, node, children):
         return str(node.value)
 
+    def visit_unnamedterm(self, node, children):
+        # children is a list of element in disjunction
+        assert len(children) == 1
+        args = children[0]
+        if args[0] == 'disjunction':
+            return args
+        return 'term', None, tuple(children)
+
+    def visit_namedterm(self, node, children):
+        assert len(children) in {1, 2}
+        if len(children) == 1:  # just an ident
+            predicate, args = children[0], ()
+        else:  # ident + args
+            predicate, args = children
+            if len(args) > 1:  # disjunction
+                args = ('disjunction', *args)
+            else:
+                assert len(args) == 1
+                args = args[0]
+        return 'term', predicate, tuple(args)
+
+    def visit_not_term(self, node, children):
+        assert len(children) == 1
+        type, predicate, args = children[0]
+        return '¬' + type, predicate, args
+
     def visit_term(self, node, children):
-        if children[0] == 'not':
-            type = '¬term'
-            predicate, *args = children[1:]
-        else:
-            type = 'term'
-            predicate, *args = children
-        assert len(args) in {0, 1}
-        args = args[0] if len(args) == 1 else args
-        return type, predicate, tuple(args)
+        assert len(children) == 1
+        type, predicate, args = children[0]
+        return type, predicate, args
+
+    def visit_not_forall(self, node, children):
+        atom, *conditions = children
+        assert len(atom) == 3
+        assert atom[0] == 'term'
+        return '¬forall', atom[1], atom[2], tuple(conditions)
 
     def visit_forall(self, node, children):
         atom, *conditions = children
         assert len(atom) == 3
-        assert atom[0] in {'term', '¬term'}
-        type = '¬forall' if atom[0] == '¬term' else 'forall'
-        return type, atom[1], atom[2], tuple(conditions)
+        assert atom[0] == 'term'
+        return 'forall', atom[1], atom[2], tuple(conditions)
 
     def visit_expression(self, node, children):
         return tuple(children)
@@ -65,7 +96,7 @@ class CodeAsTuple(ap.PTNodeVisitor):
         return 'constraint', tuple(children)
 
 
-    def visit_selection_nobody(self, node, children):
+    def visit_selection(self, node, children):
         downum, upnum, body = 0, None, ()
         if len(children) == 3:
             downum, expr, upnum = children
@@ -77,45 +108,22 @@ class CodeAsTuple(ap.PTNodeVisitor):
                 expr, upnum = children
             else:  # first number given
                 upnum, expr = children
-        return 'selection', downum, upnum, expr, body
+        return 'selection', downum, upnum, expr
 
-
-    def visit_selection_body(self, node, children):
-        downum, upnum, body = 0, None, ()
-        if len(children) == 4:
-            downum, expr, upnum, body = children
-        elif len(children) == 2:  # no numbers
-            expr, body = children
-        else:  # one number is given ; which one ?
-            assert len(children) == 3
-            tuples = tuple(isinstance(child, tuple) for child in children)
-            assert any(tuples)
-            if tuples[0]:  # the second number is given
-                assert not tuples[1]
-                expr, upnum, body = children
-            elif tuples[1]:  # the first number is given
-                downum, expr, body = children
-            else:  # no number given WTF
-                assert False, "algorithm is false."
-            # determine if body is given
-        return 'selection', downum, upnum, expr, body
 
     def visit_head(self, node, children):
         predicate, *args = children
         return children
 
     def visit_rule(self, node, children):
-        head, *body = children
+        assert len(children) == 2
+        head, body = children
         if len(head) == 1:  # general case: only one production
-            head = head[0][1:]  # remove the 'term'
+            head = head[0]
             type = 'rule'
         else:
             type = 'multirule'
-        if len(body) == 1:
-            body = body[0]
-        else:
-            raise ValueError("Body has not 1 value, but {}: {}".format(len(body), body))
-        return (type, *tuple(head), tuple(body))
+        return (type, tuple(head), tuple(body))
 
     def visit_program(self, node, children):
         for rule in children:
