5fa9046347f0af8dd7b813ad3d2a8583181e33bb
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-06 13:37:11 +0200

    smart_parser: aggregation of byline and precise parser

---
+++ clyngor/asp_parsing/smart_parser.py
@@ -0,0 +1,11 @@
+"""Aggregation of the precise parser and the byline parser,
+leading to high-level and complete parsing.
+
+"""
+
+from clyngor import asp_parsing
+from clyngor.asp_parsing import precise_parser
+from clyngor.asp_parsing import byline_parser
+
+
+
