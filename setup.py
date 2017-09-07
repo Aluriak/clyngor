4cc9ec00aac8f8999411d66ff74b95cdc6f05bed
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-07 15:42:01 +0200

    0.0.13: correct management of dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = 'clyngor',
-    version = '0.0.12',
+    version = '0.0.13',
     packages = find_packages(),
     include_package_data = True,  # read the MANIFEST.in file
     install_requires = reqs,
