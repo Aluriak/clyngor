2fcff89b4dbc0d499f920801c2be228797e1e2d8
Aluriak <lucas.bourneuf@laposte.net>
2017-09-22 22:07:26 +0200

    utils: more complete api + tests + doc

--- clyngor/utils.py
+++ clyngor/utils.py
@@ -15,10 +15,18 @@ def answer_set_to_str(answer_set:iter, atom_sep:str=' ') -> str:
     return atom_sep.join(generate_answer_set_as_str(answer_set))
 
 
-def generate_answer_set_as_str(answer_set:iter) -> iter:
+def generate_answer_set_as_str(answer_set:iter, atom_end:str='') -> iter:
     """Yield segment of string describing given answer set.
 
     answer_set -- iterable of tuple (predicate, args), or dict {predicate: [args]}
+    atom_end -- string added to the end of each given atom.
+
+    >>> '.'.join(generate_answer_set_as_str((('a', (1, 2)), ('b', ()))))
+    'a(1,2).b'
+    >>> ''.join(generate_answer_set_as_str((('a', (1, 2)), ('b', ())), atom_end='.'))
+    'a(1,2).b.'
+    >>> '1'.join(generate_answer_set_as_str((('a', (1, 2)), ('b', ())), atom_end='2'))
+    'a(1,2)21b2'
 
     """
     if isinstance(answer_set, dict):  # have been created using by_predicate, probably
@@ -28,8 +36,12 @@ def generate_answer_set_as_str(answer_set:iter) -> iter:
             for args in all_args
         )
 
+    template = '{}({})' + str(atom_end)
     for predicate, args in answer_set:
-        yield '{}({})'.format(predicate, ','.join(args)) if args else predicate
+        if args:
+            yield template.format(predicate, ','.join(map(str, args)))
+        else:
+            yield predicate + str(atom_end)
 
 
 def answer_set_from_str(line:str, collapse_atoms:bool=False,
@@ -37,6 +49,14 @@ def answer_set_from_str(line:str, collapse_atoms:bool=False,
     """Yield atoms found in given string.
 
     line -- parsable string containing an answer set
+    collapse_atoms -- whole atoms are left unparsed
+    collapse_args -- atoms args are left unparsed
+    parse_integer -- integers are returned as python int objects
+
+    >>> tuple(sorted(answer_set_from_str('a b c d', True)))
+    ('a', 'b', 'c', 'd')
+    >>> tuple(sorted((answer_set_from_str('a b(a) c("text") d', True))))
+    ('a', 'b(a)', 'c("text")', 'd')
 
     """
     yield from parsing.Parser(
@@ -46,19 +66,24 @@ def answer_set_from_str(line:str, collapse_atoms:bool=False,
     ).parse_terms(line)
 
 
-def save_answers_in_file(answers:iter, filename:str or None=None) -> str:
+def save_answers_in_file(answers:iter, filename:str or None=None,
+                         atom_separator:str='\n', end:str='') -> str:
     """Return the name of the file in which the answer sets are written.
 
     answers -- iterable of answer set to write
     filename -- file to write ; if None, a temporary file will be created
+    atom_separator -- string placed between each atom
+    end -- string written at the end
 
     """
     if not filename:
         with tempfile.NamedTemporaryFile('w', delete=False) as ofd:
             filename = ofd.name
     with open(filename, 'w') as ofd:
-        ofd.write('\n'.join(answer_set_to_str(answer) for answer in answers))
+        ofd.write(atom_separator.join(answer_set_to_str(answer)
+                                      for answer in answers) + end)
     return filename
+
 
 def load_answers_from_file(filename:str, answer_set_builder:type=frozenset) -> iter:
     """Yield answer set found in each line of given file"""
