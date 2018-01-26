267b6a995e7f2171a49b07aad9e496b75441d73c
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-01-26 18:12:47 +0100

    package now provides API for support of official clingo API

--- clyngor/__init__.py
+++ clyngor/__init__.py
@@ -1,7 +1,20 @@
 CLINGO_BIN_PATH = 'clingo'
 
 from clyngor.utils import ASPSyntaxError, ASPWarning
-from clyngor.answers import Answers
+from clyngor.answers import Answers, ClingoAnswers
 from clyngor.propagator import Propagator
 from clyngor.solving import solve, clingo_version, command
 from clyngor.inline import ASP
+
+
+def load_clingo_module() -> bool:
+    try:
+        import clingo as clingo_module
+    except ImportError:
+        clingo_module = None
+    globals()['clingo_module'] = clingo_module
+
+def have_clingo_module() -> bool:
+    return clingo_module is not None
+
+load_clingo_module()
