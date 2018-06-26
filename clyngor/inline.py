fa9882b711a42f33c89568dd217ef36fbf401c76
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-06-26 17:15:39 +0200

    ASP function got better args support + subfunctions allowing to get specific models

--- clyngor/inline.py
+++ clyngor/inline.py
@@ -6,11 +6,47 @@
 from clyngor import solve
 
 
-def ASP(source_code:str):
+def ASP(source_code:str, **kwargs):
     """Return the answer sets obtained by running the solver
     on given raw source code.
 
     source_code -- ASP source code
+    kwargs -- keyword arguments to be given to solve() call
 
     """
-    return solve(files=(), inline=source_code, stats=False)
+    return solve(files=(), inline=source_code, stats=False, **kwargs)
+
+
+def ASP_last_model(source_code:str, **kwargs):
+    """Return the last answer set obtained by running the solver
+    on given raw source code.
+
+    source_code -- ASP source code
+    kwargs -- keyword arguments to be given to solve() call
+
+    Assuming that given source code is running with an optimization
+    and with no --opt-mode option, this function returns an optimal model.
+
+    """
+    model = None
+    for model in solve(files=(), inline=source_code, stats=False, **kwargs):
+        pass
+    return model
+
+
+def ASP_one_model(source_code:str, **kwargs):
+    """Return the first answer set obtained by running the solver
+    on given raw source code.
+
+    source_code -- ASP source code
+    kwargs -- keyword arguments to be given to solve() call
+
+    """
+    models = solve(files=(), inline=source_code, stats=False, nb_model=1, **kwargs)
+    return next(models, None)
+
+
+# shortcuts
+ASP.best_model = ASP_last_model
+ASP.one_model = ASP_one_model
+ASP.all_models = ASP
