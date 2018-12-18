0b48513545f49465027d8762a0c5a51ae49edc7b
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-12-18 11:12:27 +0100

    Back to development: 0.3.12

--- clyngor/__init__.py
+++ clyngor/__init__.py
@@ -1,5 +1,5 @@
 CLINGO_BIN_PATH = 'clingo'
-__version__ = '0.3.11'
+__version__ = '0.3.12.dev0'
 
 from clyngor.utils import ASPSyntaxError, ASPWarning, clingo_value_to_python
 from clyngor.answers import Answers, ClingoAnswers
