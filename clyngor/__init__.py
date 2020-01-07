fc0efe33b38ac4d9aa666ac2e53ef2301887b764
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2020-01-07 16:51:43 +0100

    Preparing release 0.3.25

--- clyngor/__init__.py
+++ clyngor/__init__.py
@@ -1,5 +1,5 @@
 CLINGO_BIN_PATH = 'clingo'
-__version__ = '0.3.25.dev0'
+__version__ = '0.3.25'
 
 from clyngor.utils import ASPSyntaxError, ASPWarning, parse_clingo_output, clingo_value_to_python, with_clingo_bin, opt_models_from_clyngor_answers, answer_set_to_str, answer_set_from_str
 from clyngor.answers import Answers, ClingoAnswers
